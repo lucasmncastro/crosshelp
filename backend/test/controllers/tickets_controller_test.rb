e3e12dba37614ef3c7de98ca7ebd020f3acb92ae
Lucas Castro <castro.lucas@gmail.com>
2017-07-22 17:30:21 -0300

    Add REST actions for tickets controller

--- backend/test/controllers/tickets_controller_test.rb
+++ backend/test/controllers/tickets_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ticket = tickets(:one)
+  end
+
+  test "should get index" do
+    get tickets_url, as: :json
+    assert_response :success
+  end
+
+  test "should create ticket" do
+    assert_difference('Ticket.count') do
+      post tickets_url, params: { ticket: {title: "My website", user_id: users(:andrey).id} }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show ticket" do
+    get ticket_url(@ticket), as: :json
+    assert_response :success
+  end
+
+  test "should update ticket" do
+    patch ticket_url(@ticket), params: { ticket: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy ticket" do
+    assert_difference('Ticket.count', -1) do
+      delete ticket_url(@ticket), as: :json
+    end
+
+    assert_response 204
+  end
 end
