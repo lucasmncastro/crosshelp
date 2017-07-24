e86270b3926f7786cc8a068b6c1a94118ee3b9ec
Lucas Castro <castro.lucas@gmail.com>
2017-07-24 19:36:27 -0300

    Remove minitest files

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,5 +29,7 @@ module CrossHelp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators.test_framework :rspec
   end
 end
